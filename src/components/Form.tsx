2669e924922559a1c2879346621035242eaa15d1
Leo en laptop <leonar2zb@gmail.com>
2024-12-05 12:13:00 -0300

    Creando estado y enlazando valores

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,9 +1,17 @@
 import { categories } from "../data/categories"
+import { useState } from "react"
+
 export default function Form() {
+    const [activity, setActivity] = useState({
+        category: 1,
+        name: '',
+        calories: 0
+    })
+
     return (<form className="space-y-5 bg-white shadow p-10 rounded-lg">
         <div className="grid grid-cols-1 gap-3">
             <label className="font-bold" htmlFor="category">Categoría:</label>
-            <select className="border border-slate-300 p-2 rounded-lg w-full bg-white" id="category">
+            <select className="border border-slate-300 p-2 rounded-lg w-full bg-white" id="category" value={activity.category}>
                 {categories.map(category => (
                     <option value={category.id} key={category.id}>{category.name}</option>
                 ))}
@@ -11,15 +19,16 @@ export default function Form() {
         </div>
 
         <div className="grid grid-cols-1 gap-3">
-            <label className="font-bold" htmlFor="activity">Actividad:</label>
-            <input type="text" id="activity" className="border border-slate-300 p-2 rounded-lg"
-                placeholder="Ej. Comida, Jugo de Naranja, Ejercicio, Correr" />
+            <label className="font-bold" htmlFor="name">Actividad:</label>
+            <input type="text" id="name" className="border border-slate-300 p-2 rounded-lg"
+                placeholder="Ej. Comida, Jugo de Naranja, Ejercicio, Correr"
+                value={activity.name} />
         </div>
 
         <div className="grid grid-cols-1 gap-3">
             <label className="font-bold" htmlFor="calories">Calorias:</label>
             <input type="number" id="calories" className="border border-slate-300 p-2 rounded-lg"
-                placeholder="ej 300 o 500" />
+                placeholder="ej 300 o 500" value={activity.calories} />
         </div>
 
         <input type="submit"
