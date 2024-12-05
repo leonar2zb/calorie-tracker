0ef380ab74894b30f54b023a3d62775f3708a596
Leo en laptop <leonar2zb@gmail.com>
2024-12-05 12:44:34 -0300

    Permitiendo modificr datos del formulario asociados al state

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,5 +1,5 @@
 import { categories } from "../data/categories"
-import { useState } from "react"
+import React, { useState, ChangeEvent } from "react"
 
 export default function Form() {
     const [activity, setActivity] = useState({
@@ -8,10 +8,18 @@ export default function Form() {
         calories: 0
     })
 
+    const handleChange = (e: ChangeEvent<HTMLSelectElement | HTMLInputElement>) => {
+        setActivity({
+            ...activity,
+            [e.target.id]: e.target.value
+        })
+    }
+
     return (<form className="space-y-5 bg-white shadow p-10 rounded-lg">
         <div className="grid grid-cols-1 gap-3">
             <label className="font-bold" htmlFor="category">Categoría:</label>
-            <select className="border border-slate-300 p-2 rounded-lg w-full bg-white" id="category" value={activity.category}>
+            <select className="border border-slate-300 p-2 rounded-lg w-full bg-white"
+                id="category" value={activity.category} onChange={handleChange}>
                 {categories.map(category => (
                     <option value={category.id} key={category.id}>{category.name}</option>
                 ))}
@@ -22,13 +30,13 @@ export default function Form() {
             <label className="font-bold" htmlFor="name">Actividad:</label>
             <input type="text" id="name" className="border border-slate-300 p-2 rounded-lg"
                 placeholder="Ej. Comida, Jugo de Naranja, Ejercicio, Correr"
-                value={activity.name} />
+                value={activity.name} onChange={handleChange} />
         </div>
 
         <div className="grid grid-cols-1 gap-3">
             <label className="font-bold" htmlFor="calories">Calorias:</label>
             <input type="number" id="calories" className="border border-slate-300 p-2 rounded-lg"
-                placeholder="ej 300 o 500" value={activity.calories} />
+                placeholder="ej 300 o 500" value={activity.calories} onChange={handleChange} />
         </div>
 
         <input type="submit"
