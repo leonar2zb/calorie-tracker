79f772e464c3069dc1dfa2a31da1f8f5b0e56371
Leo en laptop <leonar2zb@gmail.com>
2024-12-05 11:53:00 -0300

    Creando tipos, datos y diseñando formulario

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,3 +1,31 @@
+import { categories } from "../data/categories"
 export default function Form() {
-    return (<div>Formulario</div>)
+    return (<form className="space-y-5 bg-white shadow p-10 rounded-lg">
+        <div className="grid grid-cols-1 gap-3">
+            <label className="font-bold" htmlFor="category">Categoría:</label>
+            <select className="border border-slate-300 p-2 rounded-lg w-full bg-white" id="category">
+                {categories.map(category => (
+                    <option value={category.id} key={category.id}>{category.name}</option>
+                ))}
+            </select>
+        </div>
+
+        <div className="grid grid-cols-1 gap-3">
+            <label className="font-bold" htmlFor="activity">Actividad:</label>
+            <input type="text" id="activity" className="border border-slate-300 p-2 rounded-lg"
+                placeholder="Ej. Comida, Jugo de Naranja, Ejercicio, Correr" />
+        </div>
+
+        <div className="grid grid-cols-1 gap-3">
+            <label className="font-bold" htmlFor="calories">Calorias:</label>
+            <input type="number" id="calories" className="border border-slate-300 p-2 rounded-lg"
+                placeholder="ej 300 o 500" />
+        </div>
+
+        <input type="submit"
+            className="bg-gray-800 hover:bg-gray-900 cursor-pointer p-2 w-full font-bold uppercase text-white rounded-lg"
+            value="Guardar ejercicio o comida"
+        />
+
+    </form>)
 }
