41295ac3b8b381b9e3416d9513f46ba87d52bc07
Leo en laptop <leonar2zb@gmail.com>
2024-12-06 19:30:24 -0300

    Otra forma de hacer la búsqueda

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -7,6 +7,9 @@ type ActivityListProps = {
 }
 export default function ActivityList({ activities }: ActivityListProps) {
     const categoryName = useMemo(() => (category: Activity['category']) => categories.map(cat => cat.id === category ? cat.name : ''), [activities])
+    /* Otra forma que devuelve solo el string. Dejo activa, sin embargo, la manera del curso con el array y el map
+    const categoryName = useMemo(() => (category: Activity['category']) => (categories.find(cat => cat.id === category))?.name, [activities]) */
+
     return (<>
         <h2 className="text-4xl font-bold text-slate-600 text-center">
             Comida y Actividades
@@ -14,7 +17,7 @@ export default function ActivityList({ activities }: ActivityListProps) {
                 <div key={activity.id} className="px-5 py-10 bg-white mt-5 flex justify-between">
                     <div className="space-y-2 relative">
                         <p className={`absolute -top-8 -left-8 px-10 py-2 text-white uppercase font-bold
-                            ${activity.category === 1 ? 'bg-lime-500' : 'bg-orange-500'}`}>
+                            ${+activity.category === 1 ? 'bg-lime-500' : 'bg-orange-500'}`}>
                             {categoryName(+activity.category)}
                         </p>
                         <p className="text-2xl font-bold pt-5">
