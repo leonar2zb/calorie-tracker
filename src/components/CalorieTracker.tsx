90589ad2db77e58a005c1781972d461248a56ffb
Leo en laptop <leonar2zb@gmail.com>
2024-12-11 12:12:16 -0300

    Calculando y mostrando calorías consumidas

--- src/components/CalorieTracker.tsx
+++ src/components/CalorieTracker.tsx
@@ -1,11 +1,16 @@
+import { useMemo } from "react"
 import type { Activity } from "../types"
 
 type CalorieTrackerProp = {
     activities: Activity[]
 }
 export default function CalorieTracker({ activities }: CalorieTrackerProp) {
+    const caloriesComsumed = useMemo(() => activities.reduce((total, act) => act.category === 1 ? total + act.calories : total, 0), [activities])
     return (
-        <div>
-            Calorie tracker
-        </div>)
+        <>
+            <h2 className="text-4xl font-black text-white text-center">Resumen de Calorías</h2>
+            <p className="text-white font-bold rounded-full grid grid-cols-1 gap-3 text-center">
+                <span className="font-black text-6xl text-orange-300">{caloriesComsumed}</span>
+                Consumidas</p>
+        </>)
 }
