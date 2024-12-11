825402a50c86e40f29efe8df515d557654d1d645
Leo en laptop <leonar2zb@gmail.com>
2024-12-11 12:20:32 -0300

    Calculado y mostrado las quemadas y detalles estilos

--- src/components/CalorieTracker.tsx
+++ src/components/CalorieTracker.tsx
@@ -5,12 +5,18 @@ type CalorieTrackerProp = {
     activities: Activity[]
 }
 export default function CalorieTracker({ activities }: CalorieTrackerProp) {
-    const caloriesComsumed = useMemo(() => activities.reduce((total, act) => act.category === 1 ? total + act.calories : total, 0), [activities])
+    const caloriesConsumed = useMemo(() => activities.reduce((total, act) => act.category === 1 ? total + act.calories : total, 0), [activities])
+    const caloriesBurned = useMemo(() => activities.reduce((total, act) => act.category === 2 ? total + act.calories : total, 0), [activities])
     return (
         <>
             <h2 className="text-4xl font-black text-white text-center">Resumen de Calorías</h2>
-            <p className="text-white font-bold rounded-full grid grid-cols-1 gap-3 text-center">
-                <span className="font-black text-6xl text-orange-300">{caloriesComsumed}</span>
-                Consumidas</p>
+            <div className="flex flex-col items-center md:flex-row md:justify-between gap-5 mt-10">
+                <p className="text-white font-bold rounded-full grid grid-cols-1 gap-3 text-center">
+                    <span className="font-black text-6xl text-orange-50">{caloriesConsumed}</span>
+                    Consumidas</p>
+                <p className="text-white font-bold rounded-full grid grid-cols-1 gap-3 text-center">
+                    <span className="font-black text-6xl text-orange-50">{caloriesBurned}</span>
+                    Quemadas</p>
+            </div>
         </>)
 }
