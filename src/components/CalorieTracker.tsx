53ebbb49876b9b5a1a59f1be8b2f9fcbec60ade2
Leo en laptop <leonar2zb@gmail.com>
2024-12-11 12:43:16 -0300

    Calcular la diferencia y detalle estilo Título

--- src/components/CalorieTracker.tsx
+++ src/components/CalorieTracker.tsx
@@ -8,12 +8,14 @@ type CalorieTrackerProp = {
 export default function CalorieTracker({ activities }: CalorieTrackerProp) {
     const caloriesConsumed = useMemo(() => activities.reduce((total, act) => act.category === 1 ? total + act.calories : total, 0), [activities])
     const caloriesBurned = useMemo(() => activities.reduce((total, act) => act.category === 2 ? total + act.calories : total, 0), [activities])
+    const netCalories = useMemo(() => caloriesConsumed - caloriesBurned, [activities])
     return (
         <>
             <h2 className="text-4xl font-black text-white text-center">Resumen de Calorías</h2>
             <div className="flex flex-col items-center md:flex-row md:justify-between gap-5 mt-10">
                 <CalorieDisplay calorie={caloriesConsumed} text="Consumidas" />
                 <CalorieDisplay calorie={caloriesBurned} text="Quemadas" />
+                <CalorieDisplay calorie={netCalories} text="Diferencia" />
             </div>
         </>)
 }
