7a215ca6ba658791861c7f3377909edf037170e2
Leo en laptop <leonar2zb@gmail.com>
2024-12-30 12:35:53 -0300

    Llevando uso de Context al Form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,13 +1,8 @@
 import { categories } from "../data/categories"
-import { useState, ChangeEvent, FormEvent, Dispatch, useEffect } from "react"
+import { useState, ChangeEvent, FormEvent, useEffect } from "react"
 import { v4 as uuidv4 } from "uuid"
 import type { Activity } from "../types"
-import { ActivityActions, ActivityState } from "../reducers/activity-reducer"
-
-type FormProps = {
-    dispatch: Dispatch<ActivityActions>,
-    state: ActivityState
-}
+import { useActivity } from "../types/hooks/useActivity"
 
 const initialState: Activity = {
     id: uuidv4(),
@@ -16,9 +11,9 @@ const initialState: Activity = {
     calories: 0
 }
 
-export default function Form({ dispatch, state }: FormProps) {
+export default function Form() {
     const [activity, setActivity] = useState<Activity>(initialState)
-
+    const { state, dispatch } = useActivity()
     useEffect(() => {
         if (state.activeId) {
             const selectedActivity = state.activities.find(act => act.id === state.activeId)
